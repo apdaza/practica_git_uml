82281bbd7958d6005b4a0980671005e5a9c7cd85
apdaza <apdaza@gmail.com>
2023-10-09 06:25:41 -0500

    iniciada la clase juego

---
+++ juego.py
@@ -0,0 +1,19 @@
+from mazo import *
+
+class Juego:
+    def __init__(self, mazo):
+        self.mazo = mazo
+        self.jugador1 = Mazo(True)
+        self.jugador2 = Mazo(True)
+
+    def iniciar_juego(self):
+        pass
+
+    def mostrar_juego(self):
+        pass
+
+    def valorar_juego(self):
+        pass
+
+    def jugar(self):
+        pass
